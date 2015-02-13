da8b8f132ccd7fc28d0bafacbcacfd97a28055ab
Estevan <emejmaito@gmail.com/em>
2015-02-13 18:15:16 -0200

    added post sass variaveis

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ angular.module('siteApp', [])
     self.name = 'Estevan';
     
     self.posts = [
-      {title: 'Conheça o Brackets', content: '3 motivos pelos quais o Brackets É o melhor editor de código para web atualmente.', date: 1423616325833, url: 'post/conheca-brackets.html'}
+      {title: 'Conheça o Brackets', content: '3 motivos pelos quais o Brackets É o melhor editor de código para web atualmente.', date: 1423616325833, url: 'post/conheca-brackets.html'},
+      {title: 'SASS: Como usar variáveis', content: 'Uma rápida introdução ao poderoso conceito de variáveis em SASS.', date: 1423816325833, url: 'post/sass-variaveis.html'}
     ];
   
   }]);
